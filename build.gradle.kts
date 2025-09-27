b6dd7366a797a9003e1c82c6e0e28a21ca4ec11d
moon9ibulbul <62831103+moon9ibulbul@users.noreply.github.com>
2025-09-28 01:23:17 +0700

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,4 @@
 plugins {
-    // versi ditentukan di settings.gradle.kts -> pluginManagement
     id("com.android.application") apply false
     id("com.chaquo.python") apply false
     kotlin("android") apply false
