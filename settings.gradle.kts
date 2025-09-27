1e94d5f0dd8b73842b558fda24156537df5544ba
moon9ibulbul <62831103+moon9ibulbul@users.noreply.github.com>
2025-09-28 00:30:28 +0700

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,2 +1,24 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+    plugins {
+        // Versi plugin dideklarasikan di sini
+        id("com.android.application") version "8.5.2"
+        id("com.chaquo.python") version "15.0.1"
+        kotlin("android") version "1.9.24"
+    }
+}
+
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
 rootProject.name = "StitchApp"
 include(":app")
