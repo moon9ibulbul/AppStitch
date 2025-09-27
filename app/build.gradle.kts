ffbc77ec7ae9a3c7618941a75630ddcbe8d1392b
moon9ibulbul <62831103+moon9ibulbul@users.noreply.github.com>
2025-09-28 00:11:39 +0700

    Add files via upload

---
+++ app/build.gradle.kts
@@ -0,0 +1,66 @@
+plugins {
+    id("com.android.application")
+    id("com.chaquo.python")
+    kotlin("android")
+}
+
+android {
+    namespace = "com.astral.stitchapp"
+    compileSdk = 34
+
+    defaultConfig {
+        applicationId = "com.astral.stitchapp"
+        minSdk = 26
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
+    }
+
+    buildTypes {
+        debug {
+            isMinifyEnabled = false
+        }
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+
+    buildFeatures {
+        compose = true
+    }
+    composeOptions {
+        kotlinCompilerExtensionVersion = "1.5.15"
+    }
+    packaging {
+        resources {
+            excludes += setOf("META-INF/DEPENDENCIES","META-INF/NOTICE","META-INF/LICENSE")
+        }
+    }
+}
+
+chaquopy {
+    defaultConfig {
+        buildPython("3.11")
+        pip {
+            install("pillow==10.4.0")
+            install("numpy==1.26.4")
+            install("natsort==8.4.0")
+        }
+    }
+}
+
+dependencies {
+    implementation(platform("androidx.compose:compose-bom:2024.09.02"))
+    implementation("androidx.compose.ui:ui")
+    implementation("androidx.compose.ui:ui-tooling-preview")
+    debugImplementation("androidx.compose.ui:ui-tooling")
+    implementation("androidx.compose.material3:material3")
+    implementation("androidx.activity:activity-compose:1.9.2")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.4")
+    implementation("androidx.documentfile:documentfile:1.0.1")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.8.1")
+}
