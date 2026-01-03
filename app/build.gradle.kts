dda688be7970b5ad8a63e0353b8d0ad01c19e307
moon9ibulbul <62831103+moon9ibulbul@users.noreply.github.com>
2026-01-03 11:29:34 +0700

    Update version code and name in build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "com.astral.stitchapp"
         minSdk = 26
         targetSdk = 34
-        versionCode = 4
-        versionName = "1.1.5"
+        versionCode = 5
+        versionName = "1.3.0"
 
         // Wajib untuk Chaquopy: pilih ABI yang mau dibangun
         ndk {
